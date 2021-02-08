c81997ebefda4feff716c89d0818d3c5d880d135
stepin104958 <69075307+stepin104958@users.noreply.github.com>
2021-02-08 17:19:49 +0530

    Final

---
+++ Labeeb_RelativePanelStudy/RelativePanelStudy/Views/GridView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using RelativePanelStudy.Model;
+using RelativePanelStudy.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RelativePanelStudy.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GridView : Page
+    {
+
+        
+        private ObservableCollection<BikeDetails> bikeItems_;
+        
+
+        public GridView()
+        {
+            this.InitializeComponent();
+            bikeItems_ = BikeDetails.BikeList();
+            this.DataContext = bikeItems_;
+        }
+
+        
+
+        private void AddGrid_Click(object sender, RoutedEventArgs e)
+        {
+
+            bikeItems_.Add(new BikeDetails
+            {
+                Brand = "Royal Enfield",
+                Price = 200000,
+                Model = "Classic 350",
+                Image = "/Assets/Images/bike1.png"
+            });
+        }
+
+        private void GridItem_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            
+            this.Frame.Navigate(typeof(Main));
+        }
+    }
+}
---
+++ Labeeb_RelativePanelStudy/RelativePanelStudy/Views/Main.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RelativePanelStudy.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void FlipView_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FlipView));
+        }
+
+        private void GridView_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GridView));
+        }
+    }
+}
